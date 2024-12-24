779414401415165116f6f169db644beb62181e1c
trungle0806 <trungltrh@gmail.com>
2024-12-24 16:00:14 +0700

    .

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -34,9 +34,9 @@ const Footer = () => {
     //         />
     //       </div>
     //     </div>
-          
+
     //     <div className="footer-center1">
-    //     {/* Signup Form Section */}      
+    //     {/* Signup Form Section */}
     //     <div className="footer-center">
     //       <h3 className="footer-title">Sign up for donor tips and resources</h3>
     //       <form className="footer-form">
@@ -82,75 +82,105 @@ const Footer = () => {
     //   </div>
     // </footer>
     <footer class="footer">
-  <div class="footer-container">
-    {/* <!-- Bên trái --> */}
-    <div class="footer-left">
-      <img
-        src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid_480299858/image.coreimg.svg/1672845994119/charitynav-logo-stack-white.svg"
-        alt="Charity Navigator Logo"
-        class="footer-logo"
-      />
-      <address class="footer-address">
-        299 Market Street, Suite 250<br />
-        Saddle Brook, NJ 07663
-      </address>
-      <div class="footer-certifications">
-        <img
-          src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid/image_455696160.coreimg.svg/1683206826996/four-star-rating-badge.svg"
-          alt="4-Star Rating"
-          class="certification-badge"
-        />
-        <img
-          src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid/image_455696160_copy.coreimg.svg/1657197394245/ac-s-cmyk-h-reversedwhite-w-url-01.svg"
-          alt="BBB Accredited Charity"
-          class="certification-badge"
-        />
-        <img
-          src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid/image_455696160_copy_1447565194.coreimg.85.1600.png/1730747908297/candid-seal-platinum-2024.png"
-          alt="Platinum Transparency 2024"
-          class="certification-badge"
-        />
-      </div>
-    </div>
+      <div class="footer-container">
+        <div className="footer-container-inner">
+        {/* <!-- Bên trái --> */}
+        <div class="footer-left">
+          <img
+            src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid_480299858/image.coreimg.svg/1672845994119/charitynav-logo-stack-white.svg"
+            alt="Charity Navigator Logo"
+            class="footer-logo"
+          />
+          <address class="footer-address">
+            299 Market Street, Suite 250
+            <br />
+            Saddle Brook, NJ 07663
+          </address>
+          <div class="footer-certifications">
+            <img
+              src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid/image_455696160.coreimg.svg/1683206826996/four-star-rating-badge.svg"
+              alt="4-Star Rating"
+              class="certification-badge"
+            />
+            <img
+              src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid/image_455696160_copy.coreimg.svg/1657197394245/ac-s-cmyk-h-reversedwhite-w-url-01.svg"
+              alt="BBB Accredited Charity"
+              class="certification-badge"
+            />
+            <img
+              src="https://www.charitynavigator.org/content/experience-fragments/cn/us/en/site/footer/master/_jcr_content/root/container/responsivegrid/image_455696160_copy_1447565194.coreimg.85.1600.png/1730747908297/candid-seal-platinum-2024.png"
+              alt="Platinum Transparency 2024"
+              class="certification-badge1"
+            />
+          </div>
+        </div>
 
-    {/* <!-- Bên phải --> */}
-    <div class="footer-right">
-      <h3 class="footer-title">Sign up for donor tips and resources</h3>
-      <form class="footer-form">
-        <input type="text" placeholder="First name" required />
-        <input type="text" placeholder="Last name" required />
-        <input type="email" placeholder="Email" required />
-        <button type="submit">Sign up now</button>
-      </form>
-      <div class="footer-links-group">
-        <ul class="footer-links">
-          <li><a href="#">Press Room</a></li>
-          <li><a href="#">Financials and Policies</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Terms of Use</a></li>
-        </ul>
-        <ul class="footer-links">
-          <li><a href="#">FAQs/Contact Us</a></li>
-          <li><a href="#">Our Team</a></li>
-          <li><a href="#">Careers</a></li>
-          <li><a href="#">API</a></li>
-          <li><a href="#">Sponsorship</a></li>
-        </ul>
+        {/* <!-- Bên phải --> */}
+        <div class="footer-right">
+          <h3 class="footer-title">Sign up for donor tips and resources</h3>
+          <form class="footer-form">
+            <input type="text" placeholder="First name" required />
+            <input type="text" placeholder="Last name" required />
+            <input type="email" placeholder="Email" required />
+            <button type="submit">Sign up now</button>
+          </form>
+          <div class="footer-links-group">
+            <ul class="footer-links">
+              <li>
+                <a href="#">Press Room</a>
+              </li>
+              <li>
+                <a href="#">Financials and Policies</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms of Use</a>
+              </li>
+            </ul>
+            <ul class="footer-links">
+              <li>
+                <a href="#">FAQs/Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Our Team</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
+              <li>
+                <a href="#">API</a>
+              </li>
+              <li>
+                <a href="#">Sponsorship</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-social">
+            <a href="#">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-youtube"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+        </div>
+        </div>
       </div>
-      <div class="footer-social">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-youtube"></i></a>
-        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+      <div class="footer-bottom">
+        Copyright ©2024 | EIN 13-4148824 | Bridge ID 3108588923
       </div>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    Copyright ©2024 | EIN 13-4148824 | Bridge ID 3108588923
-  </div>
-</footer>
-
+    </footer>
   );
 };
 
